9ebe35174fb2fe0f1a66c1879acdb57069c24843
Mikhail Cheshkov <mcheshkov@gmail.com>
2023-07-07 17:24:15 +0300

    Add dummy hex solution

--- code/rust/src/solution.rs
+++ code/rust/src/solution.rs
@@ -14,3 +14,25 @@ pub fn dummy(task: &Task) -> Solution {
     }
     res
 }
+
+pub fn dummy_hex(task: &Task) -> Solution {
+    let mut res = Solution::default();
+    let mut x = task.stage_left() + musician_radius;
+    let mut y = task.stage_bottom() + musician_radius;
+    let mut even = false;
+    for _m in &task.musicians {
+        res.placements.push(Coord {x, y});
+        x += 2.0 * musician_radius;
+        if !task.musician_in_stage(x, y) {
+            even = !even;
+            if even {
+                x = task.stage_left() + 2.0 * musician_radius;
+                y += 2.0 * musician_radius * 60.0f64.to_radians().sin();
+            } else {
+                x = task.stage_left() + musician_radius;
+                y += 2.0 * musician_radius;
+            }
+        }
+    }
+    res
+}
