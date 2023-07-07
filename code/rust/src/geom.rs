ecdba037909ce8eca36cb8336393015ed8fc8b10
slnpacifist <slnpacifist@gmail.com>
2023-07-07 22:43:46 +0600

    fix scoring

--- code/rust/src/geom.rs
+++ code/rust/src/geom.rs
@@ -55,19 +55,40 @@ pub struct Segment {
 
 impl Segment {
     pub fn dist(self, to: Point) -> f64 {
-        // @see https://ru.stackoverflow.com/questions/721414/Евклидова-геометрия-Расстояние-от-точки-до-отрезка
+        // @see https://habr.com/ru/articles/148325/
         let v = self.to - self.from;
         let w0 = self.from - to;
         let w1 = self.to - to;
 
-        if w0.dot(v) <= 0.0 {
+        if w0.dot(v) >= 0.0 {
             to.dist(self.from)
         } else if w1.dot(v) <= 0.0 {
             to.dist(self.to)
         } else {
-            // ((y0-y1)*x + (x0-x1)*y + (x0*y1-x1*y0))/dist(P0, P1)
-            ((self.from.y - self.to.y) * to.x + (self.from.x - self.to.x) * to.y)
-                / self.to.dist(self.from)
+            (((self.to.x - self.from.x) * (to.y - self.from.y)
+                - (self.to.y - self.from.y) * (to.x - self.from.x))
+                / self.to.dist(self.from))
+            .abs()
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Point, Segment};
+
+    fn assert_f64_eq(a: f64, b: f64) {
+        println!("{}, {}", a, b);
+        assert!((a - b).abs() < 1e-6);
+    }
+
+    #[test]
+    fn test_segment_dist() {
+        let seg = Segment {
+            from: Point { x: 0.0, y: 0.0 },
+            to: Point { x: 5.0, y: 5.0 },
+        };
+
+        assert_f64_eq(seg.dist(Point { x: 2.0, y: 2.0 }), 0.0);
     }
 }
