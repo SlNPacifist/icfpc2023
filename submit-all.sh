66e65fa6231eb169e1a5cf8bcb22e41b5a4a2288
slnpacifist <slnpacifist@gmail.com>
2023-07-08 20:58:09 +0600

    Update submit-all

--- submit-all.sh
+++ submit-all.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Uncomment following line to see executed commands
 # set -x
 
-for i in $(seq 1 55);
+for i in $(seq 1 90);
 do
     ./submit.sh "${i}" "solutions/problem-${i}.json"
 done
